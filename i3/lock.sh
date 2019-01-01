e77ec52c4ece14a6315ed5e905c0a81e146779fa
Dartmoor777 <dartmoor@mail.ua>
2019-01-02 00:13:27 +0200

    first commit

--- i3/lock.sh
+++ i3/lock.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-icon="/home/oleksandrputin/.config/i3/lock.png"
+icon="$HOME/.config/i3/lock.png"
 tmpbg="/tmp/screen.png"
 
 (( $# )) && { icon=$1; }
--- i3/startup.sh
+++ i3/startup.sh
@@ -2,10 +2,10 @@
 
 sleep 3
 
-xrandr --output VIRTUAL1 --off --output DP3 --mode 1920x1080 --pos 3280x0 --rotate normal --output eDP1 --mode 1360x768 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --primary --mode 1920x1080 --pos 1360x0 --rotate normal --output DP2 --off
-feh --bg-scale ~/.config/wall.jpg
+# xrandr --output VIRTUAL1 --off --output DP3 --mode 1920x1080 --pos 3280x0 --rotate normal --output eDP1 --mode 1360x768 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --primary --mode 1920x1080 --pos 1360x0 --rotate normal --output DP2 --off
+# feh --bg-scale ~/.config/wall.jpg
 
-nm-applet &
+nm-applet&
 
 # workspace window app
 function launch_app {
@@ -22,14 +22,9 @@ caps=`xset -q | grep Caps | awk '{ print $4 }'`
 
 #if capslock is on at login, skip running these apps at startup
 if [ $caps = off ]; then
-	launch_app 1 eDP1 terminator
-	launch_app 2 HDMI1 firefox
-	launch_app 3 HDMI1 emacs
-	launch_app 9 DP3 /home/oleksandrputin/Programs/Telegram/Telegram
-	launch_app 9 DP3 skypeforlinux
-	launch_app 8 DP3 thunderbird
-	launch_app 4 HDMI1 evince
-	launch_app 7 DP3 /home/oleksandrputin/DM/dm/dm.py
+	launch_app 1 eDP1 firefox
+	launch_app 2 eDP1 terminator
+	launch_app 9 eDP1 telegram-desktop -startintray
 else
 	#do nothing
 fi
